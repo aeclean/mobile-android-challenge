8a56a3d8cc14de47c3fcb36dcd2332676e568944
Aecio Lima <aeclean@gmail.com>
2018-06-12 02:01:24 -0300

    Create application class and test DONE!

---
+++ app/src/main/java/com/amaro/bestsellers/App.java
@@ -0,0 +1,11 @@
+package com.amaro.bestsellers;
+
+import android.app.Application;
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
